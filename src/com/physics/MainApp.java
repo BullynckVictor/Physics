9125ad685eddf26e6027044e8bcd6d6919f1014d
Victor Bullynck <victor.bullynck@gmail.com>
2021-10-28 20:38:43 +0200

    Expanded Scene structure

--- src/com/physics/MainApp.java
+++ src/com/physics/MainApp.java
@@ -3,7 +3,13 @@ package com.physics;
 import com.physics.diart.DiartScene;
 import com.physics.isaac.IsaacScene;
 import com.physics.util.DeltaTime;
+import com.physics.util.OptionsReader;
 import com.physics.victor.VictorScene;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MainApp extends Application
 {
@@ -14,7 +20,18 @@ public class MainApp extends Application
 		addScene("Isaac Development Scene", new IsaacScene());
 		addScene("Diart Development Scene", new DiartScene());
 		addScene("Victor Development Scene", new VictorScene());
-		setActiveScene("Main Development Scene");
+
+		OptionsReader options;
+
+		try {
+			options = new OptionsReader("Developer.txt");
+		}
+		catch (IOException e) {
+			options = new OptionsReader();
+		}
+
+		options.addDefault("starting scene", "Main Development Scene");
+		setActiveScene(options.getValue("starting scene"));
 	}
 
 	@Override
---
+++ src/com/physics/util/OptionsReader.java
@@ -0,0 +1,43 @@
+package com.physics.util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeMap;
+import java.util.stream.Stream;
+
+public class OptionsReader {
+
+	public OptionsReader()
+	{
+		valueMap = new TreeMap<>();
+		defaultMap = new TreeMap<>();
+	}
+
+	public OptionsReader(String filename) throws IOException {
+		valueMap = new TreeMap<>();
+		defaultMap = new TreeMap<>();
+		BufferedReader file = new BufferedReader(new FileReader("Developer.txt"));
+		Stream<String> lines = file.lines();
+		for (String line : lines.toList())
+		{
+			int index = line.indexOf(':');
+			String key = (String) line.subSequence(0, index);
+			String value = ((String) line.subSequence(index + 1, line.length())).strip();
+			valueMap.put(key, value);
+		}
+	}
+
+	public void addDefault(String key, String value)
+	{
+		defaultMap.put(key, value);
+	}
+
+	public String getValue(String key)
+	{
+		return valueMap.getOrDefault(key, defaultMap.getOrDefault(key, null));
+	}
+
+	private final TreeMap<String, String> valueMap;
+	private final TreeMap<String, String> defaultMap;
+}
