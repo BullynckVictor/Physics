861e3e6716882b6d809e2b77711af73ee679659c
EilersIs <e2004isaac@gmail.com>
2022-02-08 21:00:03 +0100

    UniversalGravityCalculator math

--- src/com/physics/UniversalGravityCalculator.java
+++ src/com/physics/UniversalGravityCalculator.java
@@ -13,6 +13,14 @@ public class UniversalGravityCalculator implements ResultantForceCalculator {
 
 	@Override
 	public void calculateResultantForce(PhysicsObject object, List<PhysicsObject> objects, int index) {
+		for(int i=0; i < objects.size(); i++) {
+			if(index != i) {
+				float rsqrd = (object.position.x - objects.get(i).position.x) * (object.position.x - objects.get(i).position.x) +
+						(object.position.y - objects.get(i).position.y) * (object.position.y - objects.get(i).position.y);
+				float UniversalG = constant * object.mass * objects.get(i).mass / rsqrd;
+				object.force.add(UniversalG*Math.abs((object.position.x - objects.get(i).position.x))/(float) Math.sqrt(rsqrd), UniversalG*Math.abs((object.position.y - objects.get(i).position.y))/(float) Math.sqrt(rsqrd));
+			}
+		}
 
 	}
 
